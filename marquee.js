abfd26b7c37cd6cab7877ba25d91a72056cd8ae6
hamzamac <hamzamac@live.com>
2016-08-01 14:26:48 +0300

    Added demonstating interface

--- marquee.js
+++ marquee.js
@@ -4,7 +4,7 @@ var ele2;
 var length1;
 var length2;
 
-// 07 speed of marquee
+
 function speed(msgLength,beltLength){
     var K=30000/1763; //constant K controls the speed of marquee, its units is time (ms)/pixel.
     if(msgLength>=beltLength){
@@ -14,16 +14,14 @@ function speed(msgLength,beltLength){
     }
 }
 
-function tangazo(message,messageLabel,msgObjId,labelObjId){
+function marquee(message,messageLabel,msgObjId,labelObjId, beltId){
     $("#"+msgObjId).empty();
     $("#"+msgObjId).html(message);
     $("#"+labelObjId).text(messageLabel);
     ele1= document.getElementById(msgObjId);
-    ele2= document.getElementById(labelObjId);
+    ele2= document.getElementById(beltId);
     length1=ele1.offsetWidth;
     length2=ele2.offsetWidth;
     $("#"+msgObjId).css("left",length2+"px");
-    $("#"+msgObjId).animate({left: ['-'+length1+'px','linear']},speed(length1,length2),function(){
-        scrol();
-    });		
+    $("#"+msgObjId).animate({left: ['-'+length1+'px','linear']},speed(length1,length2),function(){alert("Done!");});		
 }
