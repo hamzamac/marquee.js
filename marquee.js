f72c08a10d8314519c985b45a6a5e3e292d99614
hamzamac <hamzamac@live.com>
2016-08-02 11:06:09 +0300

    removal of the of the label  and finalization

--- marquee.js
+++ marquee.js
@@ -1,9 +1,4 @@
-//********************GLOBAL VARIABLES*******************//
-var ele1;
-var ele2;
-var length1;
-var length2;
-
+//******************** www.shewt.co.tz *******************//
 
 function speed(msgLength,beltLength){
     var K=30000/1763; //constant K controls the speed of marquee, its units is time (ms)/pixel.
@@ -14,14 +9,18 @@ function speed(msgLength,beltLength){
     }
 }
 
-function marquee(message,messageLabel,msgObjId,labelObjId, beltId){
+//call this fuction and provide it with the 
+//message   =the text/html to be displayed
+//msgObjId  =id of the message object (eg. div, span) cariying the text 
+//beltId    =the id of the patent object that is the background /parent of the message object
+
+function marquee(message,msgObjId, beltId){ 
     $("#"+msgObjId).empty();
     $("#"+msgObjId).html(message);
-    $("#"+labelObjId).text(messageLabel);
-    ele1= document.getElementById(msgObjId);
-    ele2= document.getElementById(beltId);
-    length1=ele1.offsetWidth;
-    length2=ele2.offsetWidth;
+    var ele1= document.getElementById(msgObjId);
+    var ele2= document.getElementById(beltId);
+    var length1=ele1.offsetWidth;
+    var length2=ele2.offsetWidth;
     $("#"+msgObjId).css("left",length2+"px");
-    $("#"+msgObjId).animate({left: ['-'+length1+'px','linear']},speed(length1,length2),function(){alert("Done!");});		
+    $("#"+msgObjId).animate({left: ['-'+length1+'px','linear']},speed(length1,length2),function(){});		
 }
